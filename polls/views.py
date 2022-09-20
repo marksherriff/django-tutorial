138420ef15719f3e10cc7b88209e536db8398572
Mark Sherriff <sherriff@virginia.edu>
2022-09-19 23:17:04 -0400

    Fixed timezone

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
